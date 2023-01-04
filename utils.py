0570dad8b2a07655d5668f1c3ce1270154216190
Guillaume Maze <gmaze@ifremer.fr>
2023-01-04 10:42:24 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -11,12 +11,13 @@ Way of using this:
 """
 
 import os
+import sys
 import warnings
 from IPython import get_ipython
 from subprocess import Popen, PIPE
-import sys
+import urllib
 
-def check_up_env():
+def check_up_env(tuto=False):
 
     def execute_this(cmd, prt=True):
         process = Popen(cmd, shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE, close_fds=True)
@@ -35,15 +36,18 @@ def check_up_env():
 
     if 'google.colab' in str(get_ipython()):
         # If we run this notebook at colab.research.google.com, we need to install more packages:
-        # !pip install --upgrade dask distributed xarray zarr gcsfs cftime nc-time-axis intake intake-xarray
         warnings.warn(
             "\nRunning on Google Colab\nBe aware that your changes won't be saved unless you save this "
             "Notebooks on your G-Drive")
         execute_this("pip install --upgrade dask distributed xarray zarr gcsfs cftime nc-time-axis intake intake-xarray",
                      prt=False);
 
-    # !curl https://raw.githubusercontent.com/obidam/ds2-2022/main/practice/exploratory_statitics/tuto_tools.py --output tuto_tools.py
-
+        # We also need to load more tools:
+        if tuto:
+            repo = "https://raw.githubusercontent.com/obidam/ds2-2023/main/"
+            urllib.request.urlretrieve(os.path.join(repo, "practice/exploratory_statistics/tuto_tools.py"),
+                                       os.path.join(".", "tuto_tools.py"))
+    
     elif os.getenv('BINDER_SERVICE_HOST'):
         warnings.warn("\nRunning on a Binder instance\nBe aware that your changes won't be saved")
     else:
